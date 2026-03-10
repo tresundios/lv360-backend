9384a1d6541dfbc0782b6c22356a3bb5209f32d9
Navis Michael Bearly J <navis@tresundios.com>
2026-03-10 16:01:37 +0530

    Moonlight

--- backend/app/main.py
+++ backend/app/main.py
@@ -73,7 +73,7 @@ app.include_router(hello_router)
 
 @app.get("/")
 def root():
-    return {"message": "Lam Viec 360 - Task CRUD API - Jenkins Push Successfully done - Team LamViec360 Dev Starlight"}
+    return {"message": "Lam Viec 360 - Task CRUD API - Jenkins Push Successfully done - Team LamViec360 Dev Moonlight"}
 
 
 @app.get("/health")
